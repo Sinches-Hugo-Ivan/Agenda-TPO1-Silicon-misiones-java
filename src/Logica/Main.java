83dfe896c1348301473e309e6a3af950022ae6ef
Sinches-Hugo-Ivan <hugo.ivan.sinches@gmail.com>
2021-11-18 16:31:47 -0300

    TPO1JAVA

---
+++ src/Logica/Main.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Logica;
+
+import IGU.Principal;
+
+/**
+ *
+ * @author SIERRA
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Principal princi = new Principal();   // Creo mi pantalla
+           princi.setVisible(true);   // Hago visible mi pantalla
+           princi.setLocationRelativeTo(null);      // Centro mi pantalla
+    }
+    
+}
